8d3a3f1758a24d57d2575772ca9b13d0d76427af
Arda Örkin <ardaorkin@gmail.com>
2022-10-16 19:35:31 +0300

    use cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import gets from "./routes/gets";
 import posts from "./routes/posts";
-
+import cors from "cors";
 var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
