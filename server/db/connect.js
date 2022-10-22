42673cef51694c3c938d8db21cbb85c50d7f3c3d
Arda Örkin <ardaorkin@gmail.com>
2022-10-22 17:50:57 +0300

    delete id rsa

--- server/db/connect.js
+++ server/db/connect.js
@@ -4,6 +4,12 @@ dotenv.config({ path: "pomodoros.env" });
 export default function connectToDB() {
   try {
     mongoose.connect(process.env.DB_CONN);
+    mongoose.connection.on("connected", () => {
+      console.log("DB connection is successfull");
+    });
+    mongoose.connection.on("error", (err) => {
+      console.error(err);
+    });
   } catch (error) {
     throw error;
   }
